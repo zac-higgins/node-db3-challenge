4b412ec6ea1ee2dd5dfaba9cac871486128fce0b
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-12-11 13:55:12 -0700

    added sanity check endpoint to server

--- server.js
+++ server.js
@@ -7,4 +7,9 @@ const server = express();
 server.use(express.json());
 server.use('/api/schemes', SchemeRouter);
 
+//----------Sanity Check----------//
+server.get('/', (req, res) => {
+    res.send('Project: node-db3-challenge is up and running!');
+});
+
 module.exports = server;
