86a20ad701f7a11965dc31e3b41cbb1d702732d6
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-12-12 08:00:35 -0700

    added first two db methods

---
+++ schemes/scheme-model.js
@@ -0,0 +1,21 @@
+const db = require('../data/db-config');
+
+module.exports = {
+    find,
+    findById,
+    // findSteps,
+    // add,
+    // update,
+    // remove
+};
+
+function find() {
+    return db('schemes');
+};
+
+function findById(id) {
+    return db('schemes')
+        .where({ id })
+        .first();
+}
+
